cc967b58224fd8ef200a33509d04729e8d62f2b2
Jason Liew <jason.97.liew@gmail.com>
2021-10-08 19:32:19 +1100

    Add welcome message for user join guild event

--- src/config.js
+++ src/config.js
@@ -1,7 +1,16 @@
+/* eslint-disable no-undef */
 require("dotenv").config()
 
 const token = process.env["DISCORD_BOT_TOKEN"]
 const notifyChannelID = process.env["NOTIFY_CHANNEL_ID"]
 const notifyUserID = process.env["NOTIFY_USER_ID"]
+const welcomeChannelID = process.env["WELCOME_CHANNEL_ID"]
+const byeChannelID = process.env["BYE_CHANNEL_ID"]
 
-module.exports = { token, notifyChannelID, notifyUserID }
+module.exports = { 
+	token, 
+	notifyChannelID, 
+	notifyUserID, 
+	welcomeChannelID, 
+	byeChannelID 
+}
--- src/server.js
+++ src/server.js
@@ -3,11 +3,11 @@ const express = require("express")
 const server = express()
 
 server.all("/", (req, res) => {
-  res.send("Your bot is alive!")
+	res.send("Your bot is alive!")
 })
 
 const keepAlive = () => {
-  server.listen(3000, () => {
+	server.listen(3000, () => {
 		console.log("Server is Ready!")
 	})
 }
